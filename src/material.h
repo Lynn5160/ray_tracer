f8c62130cb3bfcfddaad0636d056fa17ad00e77b
Sosoyan <sosoyan@sosoyan-mb.local>
2017-12-24 17:54:19 +0000

    added textures

--- src/material.h
+++ src/material.h
@@ -5,6 +5,7 @@ struct hit_record;
 
 #include "ray.h"
 #include "hitable.h"
+#include "texture.h"
 
 
 float schlick(float cosine, float ref_idx)
@@ -55,16 +56,16 @@ public:
 class lambertian : public material
 {
 public:
-    lambertian(const vec3& a) : albedo(a) {}
+    lambertian(texture* a) : albedo(a) {}
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
     {
         vec3 target = rec.p + rec.normal + random_in_unit_sphere();
         scattered = ray(rec.p, target-rec.p, r_in.time());
-        attenuation = albedo;
+        attenuation = albedo->value(0,0, rec.p);
         return true;
     }
     
-    vec3 albedo;
+    texture* albedo;
 };
 
 class metal : public material
