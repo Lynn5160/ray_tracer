5743c337999dd5332a7298ddc79e4b305be66033
Sosoyan <sosoyan@gmail.com>
2019-05-10 20:56:40 +0930

    adds ray

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <SDL2/SDL.h>
 
 #include "vec3.h"
-
+#include "ray.h"
 using namespace std;
 
 void show_window(int w, int h, unsigned int *pixels)
@@ -36,6 +36,13 @@ void show_window(int w, int h, unsigned int *pixels)
     SDL_Quit();
 }
 
+vec3 color(const ray& r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+}
+
 int main()
 {
     int nx = 512;
@@ -43,11 +50,20 @@ int main()
     
     unsigned int *pixels = new unsigned int[nx*ny];
     
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+    
     for (int j = ny-1; j >= 0; j--)
     {
         for (int i=0; i < nx; i++)
         {
-            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.0);
+            float u = float(i) / float(nx);
+            float v = float(j) / float(ny);
+            
+            ray ray(origin, lower_left_corner + (u * horizontal) + (v * vertical));
+            vec3 col = color(ray);
             
             // Converting to integers
             int ir = int(255.99 * col[0]);
