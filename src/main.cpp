e9fef9e0fce345b449bf875aeadded767480f3fb
Sosoyan <sosoyan@gmail.com>
2019-05-10 21:32:18 +0930

    adds sphere

--- src/main.cpp
+++ src/main.cpp
@@ -36,8 +36,21 @@ void show_window(int w, int h, unsigned int *pixels)
     SDL_Quit();
 }
 
+bool hit_sphere(const vec3 center, float radius, const ray& r)
+{
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 vec3 color(const ray& r)
 {
+    if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+        return vec3(1, 0, 0);
+    
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
@@ -45,7 +58,7 @@ vec3 color(const ray& r)
 
 int main()
 {
-    int nx = 512;
+    int nx = 1024;
     int ny = 512;
     
     unsigned int *pixels = new unsigned int[nx*ny];
