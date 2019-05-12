5d32585e3d0c18e0fe06bcd3131e2f6b52331683
Sosoyan <sosoyan@gmail.com>
2019-05-12 16:25:50 +0930

    adds fuzz

--- src/material.h
+++ src/material.h
@@ -33,6 +33,7 @@ class lambertian : public material
 {
     public:
         lambertian(const vec3& a) : albedo(a) {}
+        
         virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const 
          {
              vec3 target = rec.p + rec.normal + random_in_unit_sphere();
@@ -47,15 +48,18 @@ class lambertian : public material
 class metal : public material 
 {
     public:
-        metal(const vec3& a) : albedo(a) {}
+        metal(const vec3& a, float f) : albedo(a) { fuzz = f < 1 ? f : 1; }
+        
         virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const  
         {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
+        
         vec3 albedo;
+        float fuzz;
 };
 
 #endif
