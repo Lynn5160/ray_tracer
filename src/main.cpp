1c9b0f2bc49a037f406bff5def9fb8244c5ea27b
Sosoyan <sosoyan@gmail.com>
2019-06-19 14:50:44 +0400

    adds window.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,13 @@
-#include <mutex>
 #include <thread>
-#include <vector>
 
-#include <SDL2/SDL.h>
-
+#include "window.h"
 #include "ray.h"
 #include "sphere.h"
 #include "hitable_list.h"
 #include "camera.h"
 #include "material.h"
 
-
 using namespace std;
-
-
-void worker(bool* kill, int tc, int id, int nx, int ny, int ns, vector<vec3>* samples, unsigned int* pixels, hitable* world, camera* cam);
-
-
-void show_window(int w, int h, unsigned int *pixels)
-{
-    // Initialize SDL.
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-        return ;
-    
-    SDL_Window* win = SDL_CreateWindow("pRat", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Texture* img = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, w, h);
-    
-    int pitch = w * sizeof(unsigned int);
-    SDL_Event event;
-    while (true)
-    {
-        if (SDL_PollEvent(&event))
-            if (SDL_QUIT == event.type)
-                break;
-        
-        SDL_UpdateTexture(img, NULL, pixels, pitch);
-        SDL_RenderCopy(renderer, img, NULL, NULL);
-        SDL_RenderPresent(renderer);
-        this_thread::sleep_for(chrono::milliseconds(20));
-    }
-    
-    // free all resources
-    SDL_DestroyTexture(img);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-}
-
 
 vec3 color(const ray& r, hitable* world, int depth)
 {
@@ -70,16 +30,54 @@ vec3 color(const ray& r, hitable* world, int depth)
     }
 }
 
+void worker(bool* kill, int tc, int id, int width, int height, int sampling, vec3* samples, unsigned int* pixels, hitable* world, camera* cam)
+{
+    vec3 col;
+    float u, v;
+    int idx, ir, ig, ib;
+
+    int h_max = height / tc * id;
+    int h_min = h_max - (height / tc);
+    
+    for (int s = 1; s < sampling; s++)
+    {
+        for (int j = h_max-1; j >= h_min; j--)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                if (*kill)
+                    return;
+                
+                idx = width * (height-j-1) + i;
+
+                u = float(i + drand48()) / float(width);
+                v = float(j + drand48()) / float(height);
+
+                ray r = cam->get_ray(u, v);
+                
+                samples[idx] += color(r, world, 0);
+
+                col = samples[idx] / s;
+
+                // Approximate sRGB and convert to integers
+                ir = int(255.99 * sqrt(col[0]));
+                ig = int(255.99 * sqrt(col[1]));
+                ib = int(255.99 * sqrt(col[2]));
+
+                pixels[idx] = (ir << 16) + (ig << 8) + ib;
+            }
+        }
+    }
+}
 
 int main()
 {
-    int nx = 1024;
-    int ny = 512;
-    int ns = 1000;
+    int width = 1024;
+    int height = 512;
+    int sampling = 1000;
     
-    vector<vec3> samples;
-    samples.resize(nx * ny);
-    unsigned int *pixels = new unsigned int[nx * ny];
+    vec3* samples = new vec3[width * height];
+    unsigned int* pixels = new unsigned int[width * height];
 
     hitable* list[4];
     
@@ -96,11 +94,11 @@ int main()
     int threadCount = 1;
     threadCount = thread::hardware_concurrency(); // Enable Multithreading
     thread* threads = new thread[threadCount];
-    for (int id=0; id<threadCount; id++)
-        threads[id] = thread(worker, &kill, threadCount, id, nx, ny, ns, &samples, pixels, world, cam);
+    for (int id=1; id<=threadCount; id++)
+        threads[id] = thread(worker, &kill, threadCount, id, width, height, sampling, samples, pixels, world, cam);
 
     // Wait until the window is closed
-    show_window(nx, ny, pixels);
+    show_window(width, height, pixels);
     
     // Terminate threads
     kill = true;
@@ -108,46 +106,4 @@ int main()
         threads[id].join();
     
     return EXIT_SUCCESS;
-}
-
-
-void worker(bool* kill, int tc, int id, int nx, int ny, int ns, vector<vec3>* samples, unsigned int* pixels, hitable* world, camera* cam)
-{
-    int ny1 = ny / tc * (++id);
-    int ny2 = ny1 - (ny / tc);
-    
-    for (int s=0; s < ns; s++)
-    {
-        for (int j = ny1-1; j >= ny2; j--)
-        {
-            for (int i=0; i < nx; i++)
-            {
-                if (*kill)
-                    return;
-                
-                int idx = nx * (ny-j-1) + i;
-                
-                float u = float(i + drand48()) / float(nx);
-                float v = float(j + drand48()) / float(ny);
-
-                ray r = cam->get_ray(u, v);
-                vec3 col = color(r, world, 0);
-                samples->at(idx) += col;
-                
-                if (s > 0)
-                    col = samples->at(idx) / (s+1);
-                
-                // Approximte sRGB
-                col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
-
-                // Converting to integers
-                int ir = int(255.99 * col[0]);
-                int ig = int(255.99 * col[1]);
-                int ib = int(255.99 * col[2]);
-
-                lock_guard<mutex> lock(mutex);
-                pixels[idx] = (ir << 16) + (ig << 8) + ib;
-            }
-        }
-    }
 }
