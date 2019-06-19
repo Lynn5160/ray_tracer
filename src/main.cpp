5e2064641243f8876cf20973be726353c9de31aa
Sosoyan <sosoyan@gmail.com>
2019-06-19 13:39:50 +0400

    adds window.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
-#include <mutex>
 #include <thread>
-#include <SDL2/SDL.h>
 
+#include "window.h"
 #include "vec3.h"
 #include "ray.h"
 #include "sphere.h"
@@ -10,39 +9,6 @@
 #include "camera.h"
 
 using namespace std;
-
-void worker(bool* kill, int tc, int id, int nx, int ny, int ns, unsigned int* pixels, hitable* world, camera* cam);
-
-void show_window(int w, int h, unsigned int *pixels)
-{
-    // Initialize SDL.
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-        return ;
-    
-    SDL_Window* win = SDL_CreateWindow("pRat", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Texture* img = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, w, h);
-    
-    int pitch = w * sizeof(unsigned int);
-    SDL_Event event;
-    while (true)
-    {
-        if (SDL_PollEvent(&event))
-            if (SDL_QUIT == event.type)
-                break;
-        
-        SDL_UpdateTexture(img, NULL, pixels, pitch);
-        SDL_RenderCopy(renderer, img, NULL, NULL);
-        SDL_RenderPresent(renderer);
-        this_thread::sleep_for(chrono::milliseconds(20));
-    }
-    
-    // free all resources
-    SDL_DestroyTexture(img);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-}
 
 vec3 random_in_unit_sphere()
 {
@@ -68,6 +34,43 @@ vec3 color(const ray& r, hitable* world)
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5 * (unit_direction.y() + 1.0);
         return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    }
+}
+
+void worker(bool* kill, int tc, int id, int nx, int ny, int ns, unsigned int* pixels, hitable* world, camera* cam)
+{
+    int ny1 = ny / tc * (++id);
+    int ny2 = ny1 - (ny / tc);
+    
+    for (int j = ny1-1; j >= ny2; j--)
+    {
+        for (int i=0; i < nx; i++)
+        {
+            if (*kill)
+                return;
+
+            vec3 col(0, 0, 0);
+            for(int s=0; s < ns; s++)
+            {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+
+                ray r = cam->get_ray(u, v);
+                col += color(r, world);
+            }
+
+            col /= float(ns);
+            
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+
+            // Converting to integers
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
+
+            int idx = nx * (ny-j-1) + i;
+            pixels[idx] = (ir << 16) + (ig << 8) + ib;
+        }
     }
 }
 
@@ -107,42 +110,4 @@ int main()
     show_window(nx, ny, pixels);
     
     return EXIT_SUCCESS;
-}
-
-void worker(bool* kill, int tc, int id, int nx, int ny, int ns, unsigned int* pixels, hitable* world, camera* cam)
-{
-    int ny1 = ny / tc * (++id);
-    int ny2 = ny1 - (ny / tc);
-    
-    for (int j = ny1-1; j >= ny2; j--)
-    {
-        for (int i=0; i < nx; i++)
-        {
-            if (*kill)
-                return;
-
-            vec3 col(0, 0, 0);
-            for(int s=0; s < ns; s++)
-            {
-                float u = float(i + drand48()) / float(nx);
-                float v = float(j + drand48()) / float(ny);
-
-                ray r = cam->get_ray(u, v);
-                col += color(r, world);
-            }
-
-            col /= float(ns);
-            
-            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
-
-            // Converting to integers
-            int ir = int(255.99 * col[0]);
-            int ig = int(255.99 * col[1]);
-            int ib = int(255.99 * col[2]);
-
-            int idx = nx * (ny-j-1) + i;
-            lock_guard<mutex> lock(mutex);
-            pixels[idx] = (ir << 16) + (ig << 8) + ib;
-        }
-    }
 }
