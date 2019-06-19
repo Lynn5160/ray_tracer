4c6b3bcd119ee8c39f1f5b0eff8579cecc5ca5c8
Sosoyan <sosoyan@gmail.com>
2019-06-19 14:21:25 +0400

    adds widnow.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
-#include <mutex>
 #include <thread>
 #include <vector>
-#include <SDL2/SDL.h>
-
+#include "window.h"
 #include "vec3.h"
 #include "ray.h"
 #include "sphere.h"
@@ -11,39 +9,6 @@
 #include "camera.h"
 
 using namespace std;
-
-void worker(bool* kill, int tc, int id, int nx, int ny, int ns, vector<vec3>* sum_pixels, unsigned int* pixels, hitable* world, camera* cam);
-
-void show_window(int w, int h, unsigned int *pixels)
-{
-    // Initialize SDL.
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-        return ;
-    
-    SDL_Window* win = SDL_CreateWindow("pRat", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Texture* img = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, w, h);
-    
-    int pitch = w * sizeof(unsigned int);
-    SDL_Event event;
-    while (true)
-    {
-        if (SDL_PollEvent(&event))
-            if (SDL_QUIT == event.type)
-                break;
-        
-        SDL_UpdateTexture(img, NULL, pixels, pitch);
-        SDL_RenderCopy(renderer, img, NULL, NULL);
-        SDL_RenderPresent(renderer);
-        this_thread::sleep_for(chrono::milliseconds(20));
-    }
-    
-    // free all resources
-    SDL_DestroyTexture(img);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-}
 
 vec3 random_in_unit_sphere()
 {
@@ -70,43 +35,6 @@ vec3 color(const ray& r, hitable* world)
         float t = 0.5 * (unit_direction.y() + 1.0);
         return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
     }
-}
-
-int main()
-{
-    int nx = 1024;
-    int ny = 512;
-    int ns = 200;
-    
-    unsigned int *pixels = new unsigned int[nx*ny];
-    vector<vec3> sum_pixels;
-    sum_pixels.resize(nx*ny);
-    
-    hitable* list[2];
-    
-    list[0] = new sphere(vec3(0, 0, -1), 0.5);
-    list[1] = new sphere(vec3(0, -100.5, -1), 100);
-    
-    hitable* world = new hitable_list(list, 2);
-    camera* cam = new camera();
-        
-    // Spawning threads
-    bool kill = false;
-    int threadCount = 1;
-    threadCount = thread::hardware_concurrency(); // Enable Multithreading
-    thread* threads = new thread[threadCount];
-    for (int id=0; id<threadCount; id++)
-        threads[id] = thread(worker, &kill, threadCount, id, nx, ny, ns, &sum_pixels, pixels, world, cam);
-
-    // Wait until the window is closed
-    show_window(nx, ny, pixels);
-    
-    // Terminate threads
-    kill = true;
-    for (int id=0; id<threadCount; id++)
-        threads[id].join();
-    
-    return EXIT_SUCCESS;
 }
 
 void worker(bool* kill, int tc, int id, int nx, int ny, int ns, vector<vec3>* sum_pixels, unsigned int* pixels, hitable* world, camera* cam)
@@ -147,4 +75,41 @@ void worker(bool* kill, int tc, int id, int nx, int ny, int ns, vector<vec3>* su
             }
         }
     }
+}
+
+int main()
+{
+    int nx = 1024;
+    int ny = 512;
+    int ns = 200;
+    
+    unsigned int *pixels = new unsigned int[nx*ny];
+    vector<vec3> sum_pixels;
+    sum_pixels.resize(nx*ny);
+    
+    hitable* list[2];
+    
+    list[0] = new sphere(vec3(0, 0, -1), 0.5);
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    
+    hitable* world = new hitable_list(list, 2);
+    camera* cam = new camera();
+        
+    // Spawning threads
+    bool kill = false;
+    int threadCount = 1;
+    threadCount = thread::hardware_concurrency(); // Enable Multithreading
+    thread* threads = new thread[threadCount];
+    for (int id=0; id<threadCount; id++)
+        threads[id] = thread(worker, &kill, threadCount, id, nx, ny, ns, &sum_pixels, pixels, world, cam);
+
+    // Wait until the window is closed
+    show_window(nx, ny, pixels);
+    
+    // Terminate threads
+    kill = true;
+    for (int id=0; id<threadCount; id++)
+        threads[id].join();
+    
+    return EXIT_SUCCESS;
 }
