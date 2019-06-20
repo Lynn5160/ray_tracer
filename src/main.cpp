53c207f1389e5ce85ec89f3ef233396759ad414f
Sosoyan <sosoyan@gmail.com>
2019-06-20 12:51:56 +0400

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -26,8 +26,7 @@ vec3 color(const ray& r)
         return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
     }
     
-    vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5 * (r.direction().y() + 1.0);
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
