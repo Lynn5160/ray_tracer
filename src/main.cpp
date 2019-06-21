510050b5362093a95908b9ff41e61d8fd2bf8be2
Sosoyan <sosoyan@gmail.com>
2019-06-21 16:19:32 +0400

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,29 @@
 
 int main()
 {
-    int nx = 512;
-    int ny = 512;
+    int width = 512;
+    int height = 512;
     
-    unsigned int *pixels = new unsigned int[nx*ny];
+    unsigned int *pixels = new unsigned int[width * height];
     
-    for (int j = ny-1; j >= 0; j--)
+    for (int j = height-1; j >= 0; j--)
     {
-        for (int i=0; i < nx; i++)
+        for (int i=0; i < width; i++)
         {
-            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.0);
+            vec3 col(float(i) / float(width), float(j) / float(height), 0.0);
             
             // Converting to integers
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
             
-            int idx = nx * (ny-j-1) + i;
+            int idx = width * (height-j-1) + i;
             pixels[idx] = (ir << 16) + (ig << 8) + ib;
         }
     }
     
     // Wait until the window is closed
-    show_window(nx, ny, pixels);
+    show_window(width, height, pixels);
     
     return EXIT_SUCCESS;
 }
