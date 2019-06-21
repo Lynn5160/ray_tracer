f30b0219736be639b2492e4925df1e57c6fc5973
Sosoyan <sosoyan@gmail.com>
2019-06-21 16:20:42 +0400

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -12,22 +12,22 @@ vec3 color(const ray& r)
 
 int main()
 {
-    int nx = 512;
-    int ny = 512;
+    int width = 512;
+    int height = 512;
     
-    unsigned int *pixels = new unsigned int[nx*ny];
+    unsigned int *pixels = new unsigned int[width * height];
     
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
     vec3 horizontal(4.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
     
-    for (int j = ny-1; j >= 0; j--)
+    for (int j = height-1; j >= 0; j--)
     {
-        for (int i=0; i < nx; i++)
+        for (int i=0; i < width; i++)
         {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
+            float u = float(i) / float(width);
+            float v = float(j) / float(height);
             
             vec3 direction = unit_vector(lower_left_corner + (u * horizontal) + (v * vertical));
 
@@ -39,13 +39,13 @@ int main()
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
             
-            int idx = nx * (ny-j-1) + i;
+            int idx = width * (height-j-1) + i;
             pixels[idx] = (ir << 16) + (ig << 8) + ib;
         }
     }
     
     // Wait until the window is closed
-    show_window(nx, ny, pixels);
+    show_window(width, height, pixels);
     
     return EXIT_SUCCESS;
 }
